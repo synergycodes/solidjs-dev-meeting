8e837acd270c5c4da38300faf55ea03dd410b53b
Wojciech Krzesaj <wojciech.krzesaj@synergycodes.com>
2024-06-28 15:01:48 +0200

    3.2. Display first selected node key in Drawer

--- src/Drawer.tsx
+++ src/Drawer.tsx
@@ -1,5 +1,24 @@
-import { Component } from "solid-js";
+import { Component, createSignal, onCleanup, onMount } from "solid-js";
+import { diagram } from "./diagram-context";
 
 export const Drawer: Component = () => {
-  return <div class="drawer"></div>;
+  const [selection, setSelection] = createSignal<go.ObjectData | null>(null);
+
+  const listener: go.DiagramEventHandler = (event) => {
+    setSelection(event.diagram.selection.first()?.data ?? null);
+  };
+
+  onMount(() => {
+    diagram()?.addDiagramListener("ChangedSelection", listener);
+  });
+
+  onCleanup(() => {
+    diagram()?.removeDiagramListener("ChangedSelection", listener);
+  });
+
+  return (
+    <div class="drawer">
+      <span>First selected key: {JSON.stringify(selection()?.key)}</span>
+    </div>
+  );
 };
