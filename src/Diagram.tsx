47d164d6baf86444fa5f25de0b1963486c72699a
Wojciech Krzesaj <wojciech.krzesaj@synergycodes.com>
2024-06-28 14:47:33 +0200

    2.2. Use `onMount` and `onCleanup`, store diagram in signal

--- src/Diagram.tsx
+++ src/Diagram.tsx
@@ -1,10 +1,22 @@
-import { Component } from "solid-js";
+import * as go from "gojs";
+import { Component, createSignal, onCleanup, onMount } from "solid-js";
 import { createDiagram } from "./create-diagram";
 
 let diagramDiv: HTMLDivElement;
+let removeDiagram = () => {};
 
 export const Diagram: Component = () => {
-  createDiagram(diagramDiv);
+  const [diagram, setDiagram] = createSignal<go.Diagram | null>(null);
+
+  onMount(() => {
+    const data = createDiagram(diagramDiv);
+    setDiagram(data.diagram);
+    removeDiagram = data.removeDiagram;
+  });
+
+  onCleanup(() => {
+    removeDiagram();
+  });
 
   return <div ref={diagramDiv} class="diagram"></div>;
 };
